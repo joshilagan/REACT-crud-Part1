03c1881b9f4410801af67cf0323c3cf8c10c4180
joshi <joseph.ilagan.99@gmail.com>
2023-12-18 10:41:25 +0800

    .

--- src/App.js
+++ src/App.js
@@ -5,23 +5,27 @@ import './App.css';
 function App() {
   const [blogContent, setBlogContent] = useState([
     {
+    id: 1,
     title: "How to Deal with Peer Pressure as a Teenager",
     description : "Peer pressure is the influence that your friends or classmates have on your decisions and behavior. It can be positive or negative, depending on the situation. In this article, we will share some tips on how to cope with peer pressure and stay true to yourself."
     },
     {
+    id: 2,
     title: "The Benefits of Reading for Teenagers",
     description :  "Reading is not only a fun and relaxing activity, but also a great way to improve your skills and knowledge. Reading can boost your vocabulary, creativity, critical thinking, and memory. It can also expose you to different perspectives and cultures. In this article, we will explore some of the benefits of reading for teenagers and how to find books that suit your interests.",
     },
     {
+    id: 3,
     title: "The Best Apps for Teenagers in 2023", 
     description :  "Technology is an integral part of our lives, especially for teenagers. There are many apps that can help you with various aspects of your life, such as education, entertainment, health, and socializing. In this article, we will review some of the best apps for teenagers in 2023 and how they can enhance your experience."
   },
   {
-    
+    id: 4,
     title:  "How to Start a Blog as a Teenager", 
     description : "Blogging is a great way to express yourself, share your opinions, and showcase your talents. It can also help you develop your writing, communication, and marketing skills. In this article, we will guide you through the steps of starting a blog as a teenager and how to make it successful.",
   },
   {
+    id: 5,
     title: "The Challenges and Opportunities of Being a Teenager in 2023",
     description : "Being a teenager in 2023 is not easy, but also exciting. You have to face many challenges, such as academic pressure, social media, climate change, and mental health. But you also have many opportunities, such as online learning, digital entrepreneurship, social activism, and innovation. In this article, we will discuss some of the challenges and opportunities of being a teenager in 2023 and how to make the most of them.",
     }
@@ -39,12 +43,24 @@ function App() {
 
   const createPost = () => {
     const newPost = {
+      //if blog content length is 0. if true, write 1, if false, get blog length - 1, use that number as an index point to get the current id then add 1.
+      // [ 0 , 1 , 2 , 3 , 4 ]
+      //   1   2   3   4   5
+      id: blogContent.length === 0 ? 1 : blogContent[blogContent.length - 1].id + 1,
       title: newTitle,
       description: newDescription,
     }
-
     setBlogContent([...blogContent, newPost])
   }
+
+  const dels = (id) => {
+    const newContent = blogContent.filter((task)=>{
+      return task.id !== id;
+    })
+    setBlogContent(newContent)
+    }
+  
+
 
   return (
     <div className="App">
@@ -74,7 +90,7 @@ function App() {
             return(
               <div className='cards'>
                 <div className='deleteButtonDiv'>
-                  <button className='deleteButton'>x</button>
+                  <button className='deleteButton'onClick={() => dels(task.id)}>x</button>
                 </div>
                 <h3>{task.title}</h3>
                 <p>{task.description}</p>
